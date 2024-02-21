6deb72d14c8b49735e304514c0357ba8180e537a
FadimeZgj <fadime.ilhan5703@gmail.com>
2024-02-21 13:43:25 +0100

    first commit

---
+++ ValidatorProject.UnitTests/ValidatorTests.cs
@@ -0,0 +1,103 @@
+﻿using ValidatorProject;
+namespace EvaluationSampleCode.UnitTests
+{
+    [TestClass]
+    public class ValidatorTests
+    {
+        [TestMethod]
+        public void CheckNumberOfDashes_WithTwoDashes_ReturnTrue()
+        {
+            var validator = new Validator("21-02-2024");
+
+            var result = validator.CheckNumberOfDashes();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("21022024")]
+        [DataRow("2102-2024")]
+        [DataRow("21-02-20-24")]
+        [DataRow("21-02-20--24")]
+        public void CheckNumberOfDashes_WithOtherThanTwoDashes_ReturnFalse(string date)
+        {
+            var validator = new Validator(date);
+
+            var result = validator.CheckNumberOfDashes();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void CheckElementsAreNumerics_WithNumerics_ReturnTrue()
+        {
+            var validator = new Validator("21-02-2024");
+
+            var result = validator.CheckElementsAreNumerics();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow("day-02-2024")]
+        [DataRow("21-month-2024")]
+        [DataRow("21-02-year")]
+        [DataRow("day-month-year")]
+        public void CheckElementsAreNumerics_WithNonNumerics_ReturnFalse(string date)
+        {
+            var validator = new Validator(date);
+
+            var result = validator.CheckElementsAreNumerics();
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        [DataRow(31)]
+        [DataRow(21)]
+        public void CheckDayValue_WithCorrectDay_ReturnTrue(int day)
+        {
+            var validator = new Validator(String.Concat(day, "-02-2024"));
+
+            var result = validator.CheckDayValue();
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow(0)]
+        [DataRow(32)]
+        [DataRow(550)]
+        public void CheckDayValue_WithIncorrectValue_ReturnFalse(int day)
+        {
+            var validator = new Validator(String.Concat(day, "-02-2024"));
+
+            var result = validator.CheckDayValue();
+
+            Assert.IsFalse(result);
+        }
+
+        // Les tests pour CheckMonth et CheckYear ont été intentionnellement ignorés pour gagner du temps en corrigé
+
+        [TestMethod]
+        [DataRow("day-02-2024")]
+        [DataRow("21-month-2024")]
+        [DataRow("21-02-year")]
+        [DataRow("day-month-year")]
+        public void ThrowExceptionIfElementsAreNotNumeric_WithNonNumerics_ThrowsArgumentException(string date)
+        {
+            var validator = new Validator(date);
+
+            Assert.ThrowsException<ArgumentException>(() => validator.ThrowExceptionIfElementsAreNotNumeric());
+        }
+
+        [TestMethod]
+        public void ThrowExceptionIfElementsAreNotNumeric_WithNumerics_DoesntThrowException()
+        {
+            var validator = new Validator("21-02-2024");
+
+            validator.ThrowExceptionIfElementsAreNotNumeric();
+        }
+    }
+}
---
+++ ValidatorProject/Validator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidatorProject
+{
+    public class Validator
+    {
+        public string Date { get; set; }
+        private int _numberOfElementsAfterSplit = 3;
+        private string[] _splittedDate;
+        private const int dayIndex = 0;
+        private const int monthIndex = 1;
+        private const int yearIndex = 2;
+        private const int minMonthValue = 1;
+        private const int maxMonthValue = 12;
+        private const int minYearValue = 2000;
+        private readonly int maxYearValue;
+        private const int minDayValue = 1;
+        private const int maxDayValue = 31;
+
+        public Validator(string date)
+        {
+            Date = date;
+            _splittedDate = Date.Split('-');
+            maxYearValue = DateTime.Now.Year;
+        }
+
+        public bool IsValid()
+        {
+            return CheckNumberOfDashes()
+                && CheckElementsAreNumerics()
+                && CheckMonthValue()
+                && CheckYearValue()
+                && CheckDayValue();
+        }
+
+        public bool CheckNumberOfDashes()
+        {
+            return _splittedDate.Length == _numberOfElementsAfterSplit;
+        }
+
+        public bool CheckElementsAreNumerics()
+        {
+            return int.TryParse(_splittedDate[dayIndex], out _)
+                && int.TryParse(_splittedDate[monthIndex], out _)
+                && int.TryParse(_splittedDate[yearIndex], out _);
+        }
+
+        public bool CheckMonthValue()
+        {
+            ThrowExceptionIfElementsAreNotNumeric();
+
+            return Convert.ToInt32(_splittedDate[monthIndex]) >= minMonthValue
+                && Convert.ToInt32(_splittedDate[monthIndex]) <= maxMonthValue;
+        }
+
+        public bool CheckYearValue()
+        {
+            ThrowExceptionIfElementsAreNotNumeric();
+
+            return Convert.ToInt32(_splittedDate[yearIndex]) >= minYearValue
+                && Convert.ToInt32(_splittedDate[yearIndex]) <= maxYearValue;
+        }
+
+        public bool CheckDayValue()
+        {
+            ThrowExceptionIfElementsAreNotNumeric();
+
+            return Convert.ToInt32(_splittedDate[dayIndex]) >= minDayValue
+                && Convert.ToInt32(_splittedDate[dayIndex]) <= maxDayValue;
+        }
+
+        public void ThrowExceptionIfElementsAreNotNumeric()
+        {
+            if (!CheckElementsAreNumerics())
+                throw new ArgumentException("Au moins un élément n'est pas numérique");
+        }
+    }
+}
